9c0e95b856d95a68ec7f24475c45a53e6d58960b
Matvey <tok61599@gmail.com>
2025-01-25 21:05:52 +0300

    Исправление багов с кубом. Чистка кода + небольшая доработка

--- src/main.py
+++ src/main.py
@@ -6,17 +6,18 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Geometry Dash Clone")
+pygame.display.set_caption("GashGame")
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAVITY = 0.5
-JUMP_STRENGTH = 10
+JUMP_STRENGTH = 8.5
 ROTATION_SPEED = 5
 OBSTACLE_SPEED = 2
 FALL_STRENGTH = 3
 MAX_UPWARD_ROTATION = 20
 MAX_DOWNWARD_ROTATION = -20
+ROTATION_FALL_SPEED = 2
 
 
 def load_image(name, colorkey=None):
@@ -25,12 +26,14 @@ def load_image(name, colorkey=None):
         print(f"File '{fullname}' not found")
         sys.exit()
     img = pygame.image.load(fullname)
-    if colorkey is not None:
+    if colorkey:
         img.set_colorkey(colorkey)
     return img
 
 
 class GameObject:
+    """ Родительский класс для всех видов игры """
+
     def __init__(self, image_path, x, y):
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect(topleft=(x, y))
@@ -40,6 +43,8 @@ class GameObject:
 
 
 class Cube(GameObject):
+    """ Куб. баг - прокрут во время падения с платформы """
+
     def __init__(self, x, y, group):
         super().__init__("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\player_cube.png", x, y)
         self.velocity_y = 0
@@ -51,11 +56,15 @@ class Cube(GameObject):
         if not self.is_jumping:
             self.velocity_y = -JUMP_STRENGTH
             self.is_jumping = True
-            self.rotation_angle = 0
 
     def return_to_menu(self):
-        pygame.quit()
-        return False
+        return True
+
+    def correct_rotation_angle(self):
+        if self.rotation_angle < 0:
+            self.rotation_angle += 360
+        closest_angle = round(self.rotation_angle / 90) * 90
+        self.rotation_angle = closest_angle
 
     def is_falling(self):
         self.rect.y += self.velocity_y
@@ -67,12 +76,19 @@ class Cube(GameObject):
                     self.rect.bottom = surface.rect.top
                     self.is_jumping = False
                     self.velocity_y = 0
+                    self.correct_rotation_angle()
                     return True
                 elif self.rect.bottom < surface.rect.top:
                     return self.return_to_menu()
                 if self.rect.right > surface.rect.left and self.rect.left < surface.rect.right:
                     return self.return_to_menu()
 
+    def reset_position(self):
+        self.rect.x = 100
+        self.rect.y = HEIGHT - 100
+        self.velocity_y = 0
+        self.is_jumping = False
+        self.rotation_angle = 0
         return False
 
     def update_image(self):
@@ -95,13 +111,16 @@ class Cube(GameObject):
         collision_happened = self.is_falling()
 
         if self.is_jumping and not collision_happened:
-            if self.rotation_angle < 360:
-                self.rotation_angle += ROTATION_SPEED
+            self.rotation_angle += ROTATION_SPEED
+            if self.velocity_y > 0:
+                self.rotation_angle += ROTATION_FALL_SPEED
 
         self.update_image()
 
 
 class Surface(GameObject):
+    """ Платформа. баг - отображение нижней платформы (чуть выше чем нужно), может быть изменено при необходимости  """
+
     def __init__(self, x, y):
         super().__init__("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\cub.png", x, y)
         self.is_drawn = False
@@ -118,8 +137,15 @@ class Surface(GameObject):
 
 
 class Wave(GameObject):
+    """
+    Волна (стрелочка). Баг - нет адекватной работы линии - траектории движения
+    Сейчас переключается между 3-мя изображениями (с наклоном вверх / вниз / без наклона)
+    Есть некая физика. При достижении верхней границы и зажатого пробела, отображение меняется на картинку без наклона
+    Нужно поправить двойную подгрузку изображения без наклона
+    """
+
     def __init__(self, x, y):
-        super().__init__("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_straight.png", x, y)
+        super().__init__("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_straight.png", x, y) # без наклона
         self.velocity_y = 0
         self.is_moving_up = False
 
@@ -132,23 +158,23 @@ class Wave(GameObject):
         else:
             self.velocity_y = 5 if self.rect.bottom < HEIGHT else 0
 
-        self.rect.y += self.velocity_y
-
         if self.is_moving_up:
-            self.image = pygame.image.load(
-                "C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_up.png")
+            self.image = pygame.image.load("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_up.png") # наклон вверх
         else:
             if self.rect.bottom >= HEIGHT:
-                self.image = pygame.image.load(
-                    "C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_straight.png")
+                self.image = pygame.image.load("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_straight.png") # без наклона
             else:
-                self.image = pygame.image.load(
-                    "C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_down.png")
+                self.image = pygame.image.load("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\wave\\wave_down.png") # наклон вниз
 
         self.rect = self.image.get_rect(center=self.rect.center)
 
 
 class Ship(GameObject):
+    """
+    Корабль (лодка).
+    Грамотно прописана система лавирования (верх / низ), но по возможности можно сделать ее плавнее.
+    """
+
     def __init__(self, x, y):
         super().__init__("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\main_player\\player_ship.png", x, y)
         self.initial_x = x
@@ -185,6 +211,12 @@ class Ship(GameObject):
 
 
 class MainMenu:
+    """
+    Главное меню игры.
+    Пока что содержит 1 кнопку "играть". Нужно добавить переключаетли уровней и как вариант, какую то систему наград для игрока
+    (например какие-то эксклюзивные скины на куб / корабль / волну)
+    """
+
     def __init__(self):
         self.play_button = load_image("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\assets\\images\\button_play.png")
 
@@ -196,6 +228,8 @@ class MainMenu:
 
 
 def load_level(filename):
+    """ Подгрузка уровня с текстового файла. Не до конца доработано.  """
+
     surfaces = []
     line_height = 50
     sixth_line_y = 595
@@ -212,7 +246,26 @@ def load_level(filename):
     return surfaces
 
 
+def main_menu_loop(menu, cube, clock):
+    in_menu = True
+    while in_menu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                button_rect = menu.play_button.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+                if button_rect.collidepoint(event.pos):
+                    return True
+        cube.reset_position()
+        cube.group = load_level("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\levels\\lvl1.txt")
+        menu.draw()
+        clock.tick(60)
+
+
 def main():
+    """ Основная функция игры   """
+
     clock = pygame.time.Clock()
     menu = MainMenu()
     cube = Cube(100, HEIGHT - 100, [])
@@ -220,21 +273,12 @@ def main():
     in_game = False
 
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            if in_game:
+        if in_game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     cube.jump()
-            else:
-                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                    button_rect = menu.play_button.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-                    if button_rect.collidepoint(event.pos):
-                        in_game = True
-                        cube.group = load_level("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\levels\\lvl1.txt")
-
-        if in_game:
             screen.fill(WHITE)
             for surface in cube.group:
                 surface.update()
@@ -245,8 +289,16 @@ def main():
 
             if all(surface.is_drawn for surface in cube.group):
                 in_game = False
+
+            if cube.is_falling():
+                return_value = cube.is_falling()
+                if return_value:
+                    cube.reset_position()
+                    in_game = False
         else:
-            menu.draw()
+            if main_menu_loop(menu, cube, clock):
+                in_game = True
+                cube.group = load_level("C:\\Users\\Ususl\\PycharmProjects\\Dash game\\levels\\lvl1.txt")
 
         pygame.display.flip()
         clock.tick(60)
